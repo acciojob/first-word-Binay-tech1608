90b57d4a64fc88c1c373e3cec3a5fc0913f76934
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2026-06-01 19:13:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function firstWord(s) {
   // your code here
   if(s.trim().length===0){
-	  return "";
+	  return s;
   }
   else{
 	  let regex = /^\S+/;
 	  return regex.exec(s.trim()); 
   } 
-}
-
+}   
 // Do not change the code below
 const s = prompt("Enter String:");
 alert(firstWord(s));
