aae15d7fadd6d38b0d7e7cc067986552a8d76c35
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2026-06-01 18:53:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
   // your code here
+  if(s.trim().length===0){
+	  return "";
+  }
+  else{
+	  let str = s.trim();
+	  let regex = /[a-z]+\s/;
+	  return regex.exec(str);
+  }
 }
 
 // Do not change the code below
