1c195f651e6cbdc7ac8929c4cdd41f285fa727d1
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2026-06-01 19:18:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 function firstWord(s) {
-  // your code here
-  if(s.trim().length===0){
-	  return s;
-  }
-  else{
-	  let regex = /^\S+/;
-	  return regex.exec(s.trim()); 
-  } 
+  // // your code here
+  // if(s.trim().length===0){
+	 //  return s;
+  // }
+  // else{
+	 //  let regex = /^\S+/;
+	 //  return regex.exec(s.trim()); 
+  // } 
+
+
+	let str = s.trim();
+	let index = s.indexof(" ");
+	if(index === -1){
+		return str;
+	}
+	else{
+		return str.slice(0,index);
+	}
 }   
 // Do not change the code below
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
